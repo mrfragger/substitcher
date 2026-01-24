9c4c96f0d9a8097b02b22d1fa272fc52c51d3054
mrfragger <mrfragger@yahoo.com>
2026-01-24 00:55:29 -0800

    .24

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,13 @@ void main() async {
     await windowManager.ensureInitialized();
     
     WindowOptions windowOptions = const WindowOptions(
-      title: 'SubStitcher 26.01.23',
+      title: 'SubStitcher 26.01.24',
       skipTaskbar: false,
       titleBarStyle: TitleBarStyle.normal,
     );
     
     await windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.setTitle('SubStitcher 26.01.23');
+      await windowManager.setTitle('SubStitcher 26.01.24');
       await windowManager.maximize();
       await windowManager.show();
       await windowManager.focus();
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SubStitcher 26.01.23',
+      title: 'SubStitcher 26.01.24',
       themeMode: ThemeMode.dark,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
