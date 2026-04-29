c653cdd7dbc6594ab197acf81bdc7c7a8c557c2e
mrfragger <mrfragger@yahoo.com>
2026-04-28 19:40:01 -0700

    jvmTarget 17 kotlin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compilerOptions {
-    jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    kotlinOptions {
+    jvmTarget = "17"
     }
 
     defaultConfig {
